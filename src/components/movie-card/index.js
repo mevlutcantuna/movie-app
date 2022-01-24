9146dfc0e13d9671079b877c77c496c764486c47
mevlutcantuna <mttuna90@gmail.com>
2022-01-25 02:47:30 +0300

    favourite logic completed

--- src/components/movie-card/index.js
+++ src/components/movie-card/index.js
@@ -2,19 +2,52 @@ import "./style.scss";
 
 import NoMovie from "../../assets/no-movie.png";
 import IMDB from "../../assets/IMDB-icon.svg";
+import Favourite from "../../assets/favourite.svg";
+import UnFavourite from "../../assets/unfavourite.svg";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToFavourite,
+  removeFromFavourite,
+} from "../../store/actions/favourite";
 
 const MovieCard = ({ movie }) => {
+  const [isAdded, setIsAdded] = useState();
+  const favourites = useSelector((state) => state.favouriteReducer.favourites);
+
+  const dispatch = useDispatch();
+
+  const addToFav = () => {
+    dispatch(addToFavourite(movie));
+  };
+
+  const removeFromFav = () => {
+    dispatch(removeFromFavourite(movie?.id));
+  };
+
+  useEffect(() => {
+    setIsAdded(favourites?.some((item) => item.id === movie.id));
+  }, [isAdded, favourites, movie.id]);
   return (
     <div className="movie-card">
-      <img
-        className="movie-card_image"
-        src={
-          movie?.poster_path
-            ? "https://image.tmdb.org/t/p/w185" + movie?.poster_path
-            : NoMovie
-        }
-        alt="no-movie"
-      />
+      <div className="movie-card_img-logo">
+        <img
+          className="movie-card_image"
+          src={
+            movie?.poster_path
+              ? "https://image.tmdb.org/t/p/w185" + movie?.poster_path
+              : NoMovie
+          }
+          alt="no-movie"
+        />
+        <div className="movie-card_fav-buttons">
+          {isAdded ? (
+            <img onClick={removeFromFav} src={UnFavourite} alt="un-favorite" />
+          ) : (
+            <img onClick={addToFav} src={Favourite} alt="favourite" />
+          )}
+        </div>
+      </div>
       <div className="movie-card_imdb">
         <img src={IMDB} alt="imdb" />
         <span>{movie?.vote_average}</span>
